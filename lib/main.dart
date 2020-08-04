83d78d47449835ba087e902926c93ac9bc939a80
Abhijit2505 <abhijittripathy99@gmail.com>
2020-08-04 20:46:42 +0530

    Added About Me code

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 void main() {
   runApp(MyApp());
@@ -12,6 +13,7 @@ class MyApp extends StatelessWidget {
       initialRoute: '/',
       routes: {
         '/': (context) => FirstScreen(),
+        '/aboutMe' : (context) => AboutMe(),
       },
     );
   }
@@ -29,7 +31,7 @@ class FirstScreen extends StatelessWidget {
                 begin: Alignment.topLeft,
                 end: Alignment.bottomRight,
                 stops: [0.3, 1],
-                colors: [ Colors.black87,Colors.black]),
+                colors: [Colors.black87, Colors.black]),
           ),
           child: Column(
             children: <Widget>[
@@ -37,8 +39,7 @@ class FirstScreen extends StatelessWidget {
                   icon: FaIcon(FontAwesomeIcons.android),
                   color: Colors.greenAccent,
                   iconSize: 180,
-                  onPressed: () {}
-                  ),
+                  onPressed: () {}),
               SizedBox(
                 height: 20,
               ),
@@ -46,7 +47,7 @@ class FirstScreen extends StatelessWidget {
                 "Abhijit Tripathy",
                 style: TextStyle(
                   fontFamily: 'Sniglet',
-                  fontSize: 45.0,
+                  fontSize: 40.0,
                   color: Colors.white,
                   fontWeight: FontWeight.w500,
                 ),
@@ -76,7 +77,7 @@ class FirstScreen extends StatelessWidget {
                 ),
               ),
               SizedBox(
-                height:30,
+                height: 30,
               ),
               RaisedButton(
                 color: Colors.transparent,
@@ -98,8 +99,8 @@ class FirstScreen extends StatelessWidget {
                     ),
                   ),
                 ),
-                onPressed: (){
-                  print("pressed");
+                onPressed: () {
+                  Navigator.pushNamed(context, '/aboutMe');
                 },
               ),
               RaisedButton(
@@ -122,7 +123,7 @@ class FirstScreen extends StatelessWidget {
                     ),
                   ),
                 ),
-                onPressed: (){
+                onPressed: () {
                   print("pressed");
                 },
               ),
@@ -146,7 +147,7 @@ class FirstScreen extends StatelessWidget {
                     ),
                   ),
                 ),
-                onPressed: (){
+                onPressed: () {
                   print("pressed");
                 },
               ),
@@ -170,7 +171,7 @@ class FirstScreen extends StatelessWidget {
                     ),
                   ),
                 ),
-                onPressed: (){
+                onPressed: () {
                   print("pressed");
                 },
               ),
@@ -194,53 +195,46 @@ class FirstScreen extends StatelessWidget {
                     ),
                   ),
                 ),
-                onPressed: (){
+                onPressed: () {
                   print("pressed");
                 },
               ),
               SizedBox(
-                height:30,
+                height: 33,
               ),
               Row(
                 children: <Widget>[
                   SizedBox(
-                    width:49,
+                    width: 49,
                   ),
                   IconButton(
-                      icon: FaIcon(FontAwesomeIcons.twitter),
-                      color: Colors.lightBlueAccent,
-                      iconSize: 40,
-                      onPressed: () {
-                        print("Pressed");
-                      }),
+                    icon: FaIcon(FontAwesomeIcons.twitter),
+                    color: Colors.lightBlueAccent,
+                    iconSize: 40,
+                    onPressed: twitter_url,
+                  ),
                   IconButton(
-                      icon: FaIcon(FontAwesomeIcons.github),
-                      color: Colors.white,
-                      iconSize: 40,
-                      onPressed: () {
-                        print("Pressed");
-                      }),
+                    icon: FaIcon(FontAwesomeIcons.github),
+                    color: Colors.white,
+                    iconSize: 40,
+                    onPressed: github_url,
+                  ),
                   IconButton(
                       icon: FaIcon(FontAwesomeIcons.linkedin),
                       color: Colors.blue[600],
                       iconSize: 43,
-                      onPressed: () {
-                        print("Pressed");
-                      }),
+                      onPressed: linked_in_url),
                   IconButton(
                       icon: FaIcon(FontAwesomeIcons.quora),
                       color: Colors.red,
                       iconSize: 43,
-                      onPressed: () {
-                        print("Pressed");
-                      }),
+                      onPressed: quora_url),
                   IconButton(
-                      icon: FaIcon(FontAwesomeIcons.medium),
-                      color: Colors.white,
-                      iconSize: 43,
-                      onPressed: () {
-                        print("Pressed");
-                      })
+                    icon: FaIcon(FontAwesomeIcons.medium),
+                    color: Colors.white,
+                    iconSize: 43,
+                    onPressed: medium_url,
+                  ),
                 ],
               )
             ],
@@ -250,3 +244,188 @@ class FirstScreen extends StatelessWidget {
     );
   }
 }
+
+linked_in_url() async {
+  const url = 'https://www.linkedin.com/in/abhijit-tripathy-415912187/';
+  if (await canLaunch(url)) {
+    await launch(url);
+  } else {
+    throw 'Could not launch $url';
+  }
+}
+twitter_url() async {
+  const url = 'https://twitter.com/AbhijitTripat13';
+  if (await canLaunch(url)) {
+    await launch(url);
+  } else {
+    throw 'Could not launch $url';
+  }
+}
+github_url() async {
+  const url = 'https://github.com/Abhijit2505';
+  if (await canLaunch(url)) {
+    await launch(url);
+  } else {
+    throw 'Could not launch $url';
+  }
+}
+quora_url() async {
+  const url = 'https://www.quora.com/profile/Abhijit-Tripathy-9';
+  if (await canLaunch(url)) {
+    await launch(url);
+  } else {
+    throw 'Could not launch $url';
+  }
+}
+medium_url() async {
+  const url = 'https://medium.com/@abhijittripathy99';
+  if (await canLaunch(url)) {
+    await launch(url);
+  } else {
+    throw 'Could not launch $url';
+  }
+}
+
+class AboutMe extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      appBar: AppBar(
+        title: Text("ABOUT ME"),
+        backgroundColor: Colors.black87,
+      ),
+      body: SingleChildScrollView(
+        child: new Container(
+          padding: EdgeInsets.fromLTRB(0, 30, 0, 0),
+          decoration: BoxDecoration(
+            gradient: LinearGradient(
+                begin: Alignment.topLeft,
+                end: Alignment.bottomRight,
+                stops: [0.3, 1],
+                colors: [Colors.black87, Colors.black]),
+          ),
+          child: Column(
+            children: <Widget>[
+              Padding(
+                padding: const EdgeInsets.all(15.0),
+                child: Text("Who Am I ?",
+                  style : TextStyle(
+                    fontFamily: 'Sniglet',
+                    fontSize: 37,
+                    color: Colors.white,
+                    letterSpacing: 2.0,
+                    fontWeight: FontWeight.w500,
+                  ),
+                  textAlign: TextAlign.left,
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(10.0),
+                child: Text("I am an enthusiastic developer, learner, an adventurous CS geek with some designing knack. "
+                    "Also I am a strong believer that imagination brings the change and shows a path to"
+                  " develop something amazing and interesting. My interests include designing and develop sustainable "
+                    "products which can solve people's problems.",
+                style : TextStyle(
+                  fontFamily: 'Comfortaa',
+                  fontSize: 17,
+                  color: Colors.white,
+                  letterSpacing: 2.0,
+                  height: 2,
+                  fontWeight: FontWeight.w500,
+                ),
+                  textAlign: TextAlign.center,
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(15.0),
+                child: Text("What Am I ?",
+                  style : TextStyle(
+                    fontFamily: 'Sniglet',
+                    fontSize: 37,
+                    color: Colors.white,
+                    letterSpacing: 2.0,
+                    fontWeight: FontWeight.w500,
+                  ),
+                  textAlign: TextAlign.center,
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(10.0),
+                child: Text("I am a student, a developer and a designer. I am a"
+                    "part-time writer and mentor as well. I am someone who loves solitude the "
+                    "most. Solitude generates the best out of me.",
+                  style : TextStyle(
+                    fontFamily: 'Comfortaa',
+                    fontSize: 17,
+                    color: Colors.white,
+                    letterSpacing: 2.0,
+                    height: 2,
+                    fontWeight: FontWeight.w500,
+                  ),
+                  textAlign: TextAlign.center,
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(15.0),
+                child: Text("My Hobbies",
+                  style : TextStyle(
+                    fontFamily: 'Sniglet',
+                    fontSize: 37,
+                    color: Colors.white,
+                    letterSpacing: 2.0,
+                    fontWeight: FontWeight.w500,
+                  ),
+                  textAlign: TextAlign.center,
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.fromLTRB(50,0,20,0),
+                child: Row(
+                  children: <Widget>[
+                    IconButton(
+                        icon: FaIcon(FontAwesomeIcons.music),
+                        color: Colors.pinkAccent,
+                        iconSize: 30,
+                        onPressed: (){}
+                    ),
+                    IconButton(
+                        icon: FaIcon(FontAwesomeIcons.playCircle),
+                        color: Colors.yellowAccent,
+                        iconSize: 33,
+                        onPressed: (){}
+                    ),
+                    IconButton(
+                        icon: FaIcon(FontAwesomeIcons.gamepad),
+                        color: Colors.purpleAccent,
+                        iconSize: 33,
+                        onPressed: (){}
+                    ),
+                    IconButton(
+                        icon: FaIcon(FontAwesomeIcons.feather),
+                        color: Colors.greenAccent,
+                        iconSize: 33,
+                        onPressed: (){}
+                    ),
+                    IconButton(
+                        icon: FaIcon(FontAwesomeIcons.tableTennis),
+                        color: Colors.blueAccent,
+                        iconSize: 33,
+                        onPressed: (){}
+                    ),
+                    IconButton(
+                        icon: FaIcon(FontAwesomeIcons.bug),
+                        color: Colors.redAccent,
+                        iconSize: 33,
+                        onPressed: (){}
+                    ),
+                  ],
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
