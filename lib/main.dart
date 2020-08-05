4dd7b49dff218f1c3cda93a391092c68fccc4900
Abhijit2505 <abhijittripathy99@gmail.com>
2020-08-05 08:05:54 +0530

    Added the education page

--- lib/main.dart
+++ lib/main.dart
@@ -14,6 +14,7 @@ class MyApp extends StatelessWidget {
       routes: {
         '/': (context) => FirstScreen(),
         '/aboutMe' : (context) => AboutMe(),
+        '/education': (context) => EducationPage(),
       },
     );
   }
@@ -27,11 +28,14 @@ class FirstScreen extends StatelessWidget {
         child: new Container(
           padding: EdgeInsets.fromLTRB(0, 80, 0, 0),
           decoration: BoxDecoration(
-            gradient: LinearGradient(
-                begin: Alignment.topLeft,
-                end: Alignment.bottomRight,
-                stops: [0.3, 1],
-                colors: [Colors.black87, Colors.black]),
+            image: DecorationImage(
+              image: AssetImage("images/background.png"),fit: BoxFit.cover
+            ),
+//            gradient: LinearGradient(
+//                begin: Alignment.topLeft,
+//                end: Alignment.bottomRight,
+//                stops: [0.3, 1],
+//                colors: [Colors.black, Colors.black]),
           ),
           child: Column(
             children: <Widget>[
@@ -88,9 +92,10 @@ class FirstScreen extends StatelessWidget {
                     leading: Icon(
                       Icons.face,
                       color: Colors.brown,
+                      size: 35,
                     ),
                     title: Text(
-                      '      ABOUT ME',
+                      'ABOUT ME',
                       style: TextStyle(
                         fontSize: 20,
                         fontWeight: FontWeight.w500,
@@ -112,9 +117,10 @@ class FirstScreen extends StatelessWidget {
                     leading: Icon(
                       Icons.school,
                       color: Colors.blue[900],
+                      size: 35,
                     ),
                     title: Text(
-                      "   MY EDUCATION",
+                      "MY EDUCATION",
                       style: TextStyle(
                         fontSize: 20,
                         fontWeight: FontWeight.w500,
@@ -124,7 +130,7 @@ class FirstScreen extends StatelessWidget {
                   ),
                 ),
                 onPressed: () {
-                  print("pressed");
+                  Navigator.pushNamed(context, '/education');
                 },
               ),
               RaisedButton(
@@ -136,9 +142,10 @@ class FirstScreen extends StatelessWidget {
                     leading: Icon(
                       Icons.work,
                       color: Colors.green,
+                      size: 35,
                     ),
                     title: Text(
-                      ' WORK EXPERIENCE',
+                      'WORK EXPERIENCE',
                       style: TextStyle(
                         fontSize: 20,
                         fontWeight: FontWeight.w500,
@@ -160,9 +167,10 @@ class FirstScreen extends StatelessWidget {
                     leading: Icon(
                       Icons.code,
                       color: Colors.black,
+                      size: 35,
                     ),
                     title: Text(
-                      '       MY SKILLS',
+                      'MY SKILLS',
                       style: TextStyle(
                         fontSize: 20,
                         fontWeight: FontWeight.w500,
@@ -184,9 +192,10 @@ class FirstScreen extends StatelessWidget {
                     leading: Icon(
                       Icons.dashboard,
                       color: Colors.pink,
+                      size: 35,
                     ),
                     title: Text(
-                      '    MY PROJECTS',
+                      'MY PROJECTS',
                       style: TextStyle(
                         fontSize: 20,
                         fontWeight: FontWeight.w500,
@@ -200,12 +209,12 @@ class FirstScreen extends StatelessWidget {
                 },
               ),
               SizedBox(
-                height: 33,
+                height: 29,
               ),
               Row(
                 children: <Widget>[
                   SizedBox(
-                    width: 49,
+                    width: 65,
                   ),
                   IconButton(
                     icon: FaIcon(FontAwesomeIcons.twitter),
@@ -292,17 +301,20 @@ class AboutMe extends StatelessWidget {
     return new Scaffold(
       appBar: AppBar(
         title: Text("ABOUT ME"),
-        backgroundColor: Colors.black87,
+        backgroundColor: Colors.black,
       ),
       body: SingleChildScrollView(
         child: new Container(
-          padding: EdgeInsets.fromLTRB(0, 30, 0, 0),
+          padding: EdgeInsets.fromLTRB(0, 25, 0, 0),
           decoration: BoxDecoration(
-            gradient: LinearGradient(
-                begin: Alignment.topLeft,
-                end: Alignment.bottomRight,
-                stops: [0.3, 1],
-                colors: [Colors.black87, Colors.black]),
+            image: DecorationImage(
+                image: AssetImage("images/aboutus.png"),fit: BoxFit.cover
+            ),
+//            gradient: LinearGradient(
+//                begin: Alignment.topLeft,
+//                end: Alignment.bottomRight,
+//                stops: [0.3, 1],
+//                colors: [Colors.black87, Colors.black]),
           ),
           child: Column(
             children: <Widget>[
@@ -323,7 +335,7 @@ class AboutMe extends StatelessWidget {
                 padding: const EdgeInsets.all(10.0),
                 child: Text("I am an enthusiastic developer, learner, an adventurous CS geek with some designing knack. "
                     "Also I am a strong believer that imagination brings the change and shows a path to"
-                  " develop something amazing and interesting. My interests include designing and develop sustainable "
+                  " develop something amazing and interesting. My interests include designing and developing sustainable "
                     "products which can solve people's problems.",
                 style : TextStyle(
                   fontFamily: 'Comfortaa',
@@ -420,7 +432,47 @@ class AboutMe extends StatelessWidget {
                     ),
                   ],
                 ),
-              )
+              ),
+              SizedBox(
+                height: 10,
+              ),
+              Padding(
+                padding: const EdgeInsets.all(15.0),
+                child: Text("My Goals In Life",
+                  style : TextStyle(
+                    fontFamily: 'Sniglet',
+                    fontSize: 37,
+                    color: Colors.white,
+                    letterSpacing: 2.0,
+                    fontWeight: FontWeight.w500,
+                  ),
+                  textAlign: TextAlign.center,
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(10.0),
+                child: Text("In everyday life I witness a lot of problems in my "
+                    "surrounding that can be solved using technology. My goal in"
+                    "life is to design something good, that can solve people's problem and "
+                    "can inspire the upcoing generation as well.",
+                  style : TextStyle(
+                    fontFamily: 'Comfortaa',
+                    fontSize: 17,
+                    color: Colors.white,
+                    letterSpacing: 2.0,
+                    height: 2,
+                    fontWeight: FontWeight.w500,
+                  ),
+                  textAlign: TextAlign.center,
+                ),
+              ),
+              SizedBox(
+                height: 40,
+                width: 260,
+                child: Divider(
+                  color: Colors.redAccent,
+                ),
+              ),
             ],
           ),
         ),
@@ -429,3 +481,157 @@ class AboutMe extends StatelessWidget {
   }
 }
 
+class EducationPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      appBar: AppBar(
+        title: Text(
+          "MY EDUCATION"
+        ),
+        backgroundColor: Colors.black,
+      ),
+      body: SingleChildScrollView(
+        child: new Container(
+          padding: EdgeInsets.fromLTRB(0, 25, 0, 0),
+          decoration: BoxDecoration(
+            image: DecorationImage(
+                image: AssetImage("images/education.png"),fit: BoxFit.cover
+            ),
+          ),
+          child: Column(
+            children: <Widget>[
+              Center(
+                child: IconButton(
+                  icon: FaIcon(FontAwesomeIcons.university),
+                  iconSize: 80,
+                  color: Colors.white,
+                  onPressed: (){},
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(15.0),
+                child: Text("GURU GHASIDAS VISHWAVIDYALYA, BILASPUR",
+                  style : TextStyle(
+                    fontFamily: 'Sniglet',
+                    fontSize: 27,
+                    color: Colors.white,
+                    letterSpacing: 2.0,
+                    fontWeight: FontWeight.w500,
+                  ),
+                  textAlign: TextAlign.center,
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(5.0),
+                child: Text("B.Tech, Computer Science And Engineering\n"
+                    "(2019-2023)\n\n"
+                    "I am currently an undergraduate and a junior developer majoring in CSE, "
+                    "more specifically product development and deployment in distributed system.",
+                  style : TextStyle(
+                    fontFamily: 'Comfortaa',
+                    fontSize: 17,
+                    color: Colors.white,
+                    letterSpacing: 2.0,
+                    height: 2,
+                    fontWeight: FontWeight.w500,
+                  ),
+                  textAlign: TextAlign.center,
+                ),
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              Center(
+                child: IconButton(
+                  icon: FaIcon(FontAwesomeIcons.building),
+                  iconSize: 80,
+                  color: Colors.white,
+                  onPressed: (){},
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(15.0),
+                child: Text("B.J.B. JUNIOR COLLEGE, BHUBANESHWAR",
+                  style : TextStyle(
+                    fontFamily: 'Sniglet',
+                    fontSize: 27,
+                    color: Colors.white,
+                    letterSpacing: 2.0,
+                    fontWeight: FontWeight.w500,
+                  ),
+                  textAlign: TextAlign.center,
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(5.0),
+                child: Text("Intermediate Of Science, PCMSt\n"
+                    "(2015-2017)\n\n"
+                    "I have completed my schooling majoring in PCM and Statistics."
+                    "Also I was the topper of statistics in state. ",
+                  style : TextStyle(
+                    fontFamily: 'Comfortaa',
+                    fontSize: 17,
+                    color: Colors.white,
+                    letterSpacing: 2.0,
+                    height: 2,
+                    fontWeight: FontWeight.w500,
+                  ),
+                  textAlign: TextAlign.center,
+                ),
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              Center(
+                child: IconButton(
+                  icon: FaIcon(FontAwesomeIcons.school),
+                  iconSize: 80,
+                  color: Colors.white,
+                  onPressed: (){},
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(15.0),
+                child: Text("SARASWATI SISHU VIDYA MANDIR, SUNDERGARH",
+                  style : TextStyle(
+                    fontFamily: 'Sniglet',
+                    fontSize: 27,
+                    color: Colors.white,
+                    letterSpacing: 2.0,
+                    fontWeight: FontWeight.w500,
+                  ),
+                  textAlign: TextAlign.center,
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(5.0),
+                child: Text("PRIMARY & SECONDARY SCHOOLING\n"
+                    "(2007-2015)\n\n"
+                    "I was the topper of my school as well as my district in the matriculation"
+                    " exam by BSE, Odisha.",
+                  style : TextStyle(
+                    fontFamily: 'Comfortaa',
+                    fontSize: 17,
+                    color: Colors.white,
+                    letterSpacing: 2.0,
+                    height: 2,
+                    fontWeight: FontWeight.w500,
+                  ),
+                  textAlign: TextAlign.center,
+                ),
+              ),
+              SizedBox(
+                height: 80,
+                width: 260,
+                child: Divider(
+                  color: Colors.redAccent,
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
